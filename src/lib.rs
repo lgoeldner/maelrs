8f2817f9eae474056b150b55e927827406446ebe
lgoeldner <linus@goeldner-family.de>
2025-02-16 21:19:03 +0100

    echo and GUID workloads working, implemented RPC

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use log::error;
 use serde::{Deserialize, Serialize};
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
 
 #[derive(Debug)]
 pub enum Error {
@@ -10,6 +10,9 @@ pub enum Error {
     SendFailed,
     SerializeFailed,
     InitFailed,
+    TimeError,
+    RPCFailed,
+    NoRPCHandler,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -28,7 +31,7 @@ pub struct Body {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(tag = "type", rename = "snake_case")]
+#[serde(tag = "type", rename_all = "snake_case")]
 pub enum Payload {
     Echo {
         echo: String,
@@ -41,21 +44,69 @@ pub enum Payload {
         node_id: String,
         node_ids: Vec<String>,
     },
+    Generate,
+    GenerateOk {
+        id: u128,
+    },
 }
+
+pub type RegisterCallbackSender = mpsc::Sender<(u32, oneshot::Sender<Message>)>;
 
 /// gets created for every incoming message and passed to the handler function
 /// abstraction for shared state and the RPC protocol
 #[derive(Debug)]
 pub struct Request {
+    // when a message gets sent through this, the corresponding `oneshot::Receiver<_>`
+    // can expect to be calles when a reply is available
+    register_rpc_callback: RegisterCallbackSender,
     send: mpsc::Sender<String>,
     message: Message,
+    this_node_id: u32,
 }
 
 impl Request {
-    pub fn new(send: mpsc::Sender<String>, message: Message) -> Self {
-        Self { send, message }
+    pub fn new(
+        send: mpsc::Sender<String>,
+        message: Message,
+        this_node_id: u32,
+        register_rpc_callback: RegisterCallbackSender,
+    ) -> Self {
+        Self {
+            send,
+            message,
+            this_node_id,
+            register_rpc_callback,
+        }
     }
 
+    /// send a message awaiting a response
+    /// returns the response when it arrives
+    pub async fn rpc(&self, payload: Payload) -> Result<Message, Error> {
+        // first, send the reply
+        self.reply(payload).await?;
+
+        // then wait for the response by registering a oneshot callback channel
+        let (tx, rx) = oneshot::channel();
+        self.register_rpc_callback
+            .send((self.message.body.msg_id, tx))
+            .await
+            .map_err(|e| {
+                error!(
+                "register rpc callback sender was dropped! RPC from msg {:?} failed due to {e:?}",
+                self.message
+            );
+                Error::SendFailed
+            })?;
+
+        rx.await.map_err(|e| {
+            error!("awaiting RPC response failed due to {e:?}");
+            Error::RPCFailed
+        })
+    }
+
+    /// reply to the message (`self.message()`)
+    /// with custom payload
+    /// handles everything else
     pub async fn reply(&self, payload: Payload) -> Result<(), Error> {
         let reply_to = self.message();
         let msg = Message {
@@ -92,6 +143,10 @@ impl Request {
 
     pub fn message_payload(&self) -> &Payload {
         &self.message.body.payload
+    }
+
+    pub fn this_node_id(&self) -> u32 {
+        self.this_node_id
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use log::{error, info};
-use maelrs::{Body, Error, Message, Payload, Request};
+use env_logger::Target;
+use log::{error, info, warn, LevelFilter};
+use maelrs::{Body, Error, Message, Payload, RegisterCallbackSender, Request};
+use rand::RngCore;
+use std::collections::HashMap;
 use std::io;
+use std::time::SystemTime;
 use tokio::sync::mpsc::Sender;
+use tokio::sync::oneshot;
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     sync::mpsc::{self, Receiver},
@@ -9,39 +14,111 @@ use tokio::{
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    eprintln!("startup");
     // setup logging to stderr
-    simple_logger::SimpleLogger::new().init().unwrap();
+    env_logger::builder()
+        .filter_level(LevelFilter::max())
+        .init();
+
+    warn!("start");
+
     // create task that receives JSON message and writes it to stdout
     // also channel that gets passed to every
-    let (tx, rx) = mpsc::channel(10);
-    tokio::spawn(sender_task(rx));
+    let (reply_tx, reply_rx) = mpsc::channel(10);
+    tokio::spawn(sender_task(reply_rx));
 
     // each line in stdin is a new JSON message
     let mut lines = BufReader::new(tokio::io::stdin()).lines();
+
+    // the dictionary of messages that expect replies to a
+    // `oneshot::Sender<_>` that will get called when a response comes in
+    let mut waiting_for_response: HashMap<u32, oneshot::Sender<Message>> =
+        HashMap::with_capacity(1024);
+    // channel to register a task waiting for a response
+    let (rpc_tx, mut rpc_rx): (RegisterCallbackSender, _) = mpsc::channel(10);
 
     // the first message is the `init` message
     // => https://github.com/jepsen-io/maelstrom/blob/main/doc/protocol.md
     // we handle that specially so the data we receive in it can be made available in Requests
     let init_msg = lines.next_line().await;
-    let (_this_node, _other_nodes) = handle_init_message(&tx, init_msg).await?;
+    let (this_node, _other_nodes) = handle_init_message(&reply_tx, init_msg, &rpc_tx).await?;
 
-    // wait for each message to come in
-    while let Ok(Some(next_line)) = lines.next_line().await {
-        // parse the message
-        let msg: Message = match serde_json::from_str(&next_line) {
-            Ok(o) => o,
-            Err(e) => {
-                error!("Could not parse line! err: {e}");
-                continue;
+    info!("init complete");
+
+    loop {
+        // wait for each message to come in
+        // also add any incoming rpc callbacks
+        let next_line = tokio::select! {
+            Some((id, callback)) = rpc_rx.recv() => {
+                info!("received callback waiting on {id}");
+                waiting_for_response.insert(id, callback);
+                None
+            },
+
+            next_line = lines.next_line() => {
+                next_line.unwrap_or_else(|e| {
+                        error!("failed to read next message due to {e}");
+                        None
+                    })
             }
         };
 
+        if let Some(s) = next_line {
+            match dispatch_message(
+                reply_tx.clone(),
+                rpc_tx.clone(),
+                this_node,
+                &s,
+                &mut waiting_for_response,
+            ) {
+                Ok(()) => {}
+                Err(e) => {
+                    error!("Error while dispatching message: {e:?}");
+                }
+            }
+        }
+    }
+}
+
+fn dispatch_message(
+    tx: Sender<String>,
+    rpc_tx: RegisterCallbackSender,
+    this_node: u32,
+    next_line: &String,
+    handlers: &mut HashMap<u32, oneshot::Sender<Message>>,
+) -> Result<(), Error> {
+    // parse the message
+    let msg: Message = match serde_json::from_str(&next_line) {
+        Ok(o) => o,
+        Err(e) => {
+            error!("Could not parse line! err: {e}");
+            return Err(Error::SerializeFailed);
+        }
+    };
+
+    if let Some(reply_to) = msg.body.in_reply_to {
+        // this is a RPC message
+        // search for the corresponding handler
+        match handlers.remove(&reply_to) {
+            Some(reply) => match reply.send(msg) {
+                Ok(_) => {}
+                Err(msg) => {
+                    error!("RPC failed, msg: {msg:?}");
+                    return Err(Error::RPCFailed);
+                }
+            },
+            None => {
+                error!("no handler for reply to {reply_to}");
+                return Err(Error::NoRPCHandler);
+            }
+        }
+    } else {
         // and create the request object
-        let req = Request::new(tx.clone(), msg);
+        let req = Request::new(tx.clone(), msg, this_node, rpc_tx.clone());
 
         // finally, spawn a task to handle the message
         // TODO: proper error reporting
-        let _ = tokio::spawn(handle_msg(req));
+        tokio::spawn(handle_msg(req));
     }
 
     Ok(())
@@ -50,8 +127,9 @@ async fn main() -> Result<(), Error> {
 async fn handle_init_message(
     reply_channel: &Sender<String>,
     init_msg: io::Result<Option<String>>,
-) -> Result<(String, Vec<String>), Error> {
-    let res = match init_msg {
+    rpc_channel: &RegisterCallbackSender,
+) -> Result<(u32, Vec<String>), Error> {
+    match init_msg {
         Ok(Some(json)) => {
             let msg = serde_json::from_str::<Message>(&json);
             match msg {
@@ -62,10 +140,27 @@ async fn handle_init_message(
                 Ok(msg) => {
                     match &msg.body.payload {
                         Payload::Init { node_id, node_ids } => {
+                            // extract the number part and the "n" descriptor from the node id
+                            let Some(("n", to_parse)) = node_id.split_at_checked(1) else {
+                                error!("failed to parse self node id from {node_id}");
+                                return Err(Error::InitFailed);
+                            };
+
+                            // then parse into an int
+                            let this_id: u32 = to_parse.parse().map_err(|e| {
+                                error!("failed to parse node id: {e:?}");
+                                Error::InitFailed
+                            })?;
+
                             // copy the needed data
-                            let ret = (node_id.clone(), node_ids.clone());
+                            let ret = (this_id, node_ids.clone());
                             // make a request so its easier to reply
-                            let req = Request::new(reply_channel.clone(), msg);
+                            let req = Request::new(
+                                reply_channel.clone(),
+                                msg,
+                                this_id,
+                                rpc_channel.clone(),
+                            );
                             req.reply(Payload::InitOk).await?;
 
                             info!("init is ok!");
@@ -87,32 +182,60 @@ async fn handle_init_message(
             error!("failed to receive init message due to {e}");
             Err(Error::InitFailed)
         }
-    };
-
-    res
+    }
 }
 
 async fn handle_msg(req: Request) -> Result<(), Error> {
     match req.message_payload() {
+        // just echo the same message back
         Payload::Echo { echo } => req.reply(Payload::EchoOk { echo: echo.clone() }).await,
+        Payload::Generate => {
+            // get the millis since the epoch
+            let since_epoch = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+                Ok(o) => o,
+                Err(_) => {
+                    error!("system time is set before the Unix Epoch");
+                    return Err(Error::TimeError);
+                }
+            };
+
+            let millis = since_epoch.as_millis();
+
+            // combine the millis with a random number
+            let random_num = rand::rng().next_u32();
+            let guid = (millis << 16) ^ random_num as u128;
+
+            req.reply(Payload::GenerateOk { id: guid }).await
+        }
 
         Payload::Init { .. } => {
             // is handled at setup
             panic!("init message in `handle_msg`");
         }
         _ => {
-            error!("cant handle {req:?}!");
+            error!("cant handle payload {req:?}!");
             Err(Error::CantHandleMsg)
         }
     }
 }
 
 async fn sender_task(mut rx: Receiver<String>) -> Result<(), Error> {
+    info!("started sender task!");
     let mut stdout = tokio::io::stdout();
     while let Some(to_send) = rx.recv().await {
+        info!("to send: {to_send}");
+        // first, write all the json data
         let res = stdout.write_all(to_send.as_bytes()).await;
+
+        // then write a newline to finish the message and flush the buffer
+        let newline_res = stdout.write_u8(b'\n').await;
+        info!("sent!");
         if let Err(e) = res {
-            error!("failed to write message to stdout!")
+            error!("failed to write message {to_send} to stdout due to {e}")
+        }
+
+        if let Err(e) = newline_res {
+            error!("failed to finish message {to_send} to stdout due to {e}")
         }
     }
     Ok(())
